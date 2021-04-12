73e809b00208e7ca840e9a19ceb0edd95c9c4fae
Venanzio Capretta <venanzio@duplavis.com>
2021-04-12 17:57:17 +0100

    merge-sort implementation in Coq
    I did it in Coq because I still haven't fixed my Agda installation
    I assume the splitting and merging functions as variables:
      the structure of the implementation doesn't depend on how they're defined

---
+++ mergesort.v
@@ -0,0 +1,26 @@
+Module Merge_Sort.
+
+Variable A: Set.
+
+Variable split_list : list A -> (list A) * (list A).
+
+Definition first_half : list A -> list A := fun l => fst (split_list l).
+
+Definition second_half : list A -> list A := fun l => snd (split_list l).
+
+Inductive merge_Dom: list A -> Set :=
+  mD_nil: merge_Dom nil
+| mD_split: forall l, merge_Dom (first_half l) -> merge_Dom (second_half l) 
+                      -> merge_Dom l.
+
+Variable merge: list A -> list A -> list A.
+
+Fixpoint mergeD_sort (l:list A)(h: merge_Dom l): list A :=
+match h with
+  mD_nil => nil
+| mD_split l h1 h2 => merge (mergeD_sort (first_half l) h1)
+                            (mergeD_sort (second_half l) h2)
+end.
+
+End Merge_Sort.
+
