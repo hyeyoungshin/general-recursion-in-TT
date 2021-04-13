3aaed545c160982b2b8b918d635d0ebce3db4f9a
Venanzio Capretta <venanzio@duplavis.com>
2021-04-13 08:39:12 +0100

    Added case for singleton list

--- mergesort.v
+++ mergesort.v
@@ -10,6 +10,7 @@ Definition second_half : list A -> list A := fun l => snd (split_list l).
 
 Inductive merge_Dom: list A -> Set :=
   mD_nil: merge_Dom nil
+| mD_single: forall x:A, merge_Dom (cons x nil)
 | mD_split: forall l, merge_Dom (first_half l) -> merge_Dom (second_half l) 
                       -> merge_Dom l.
 
@@ -18,6 +19,7 @@ Variable merge: list A -> list A -> list A.
 Fixpoint mergeD_sort (l:list A)(h: merge_Dom l): list A :=
 match h with
   mD_nil => nil
+| mD_single x => cons x nil
 | mD_split l h1 h2 => merge (mergeD_sort (first_half l) h1)
                             (mergeD_sort (second_half l) h2)
 end.
