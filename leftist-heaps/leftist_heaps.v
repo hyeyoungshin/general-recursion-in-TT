248f26a5b9fb51b70fa1c16c320aeea9cf1ed6cc
Venanzio Capretta <venanzio@duplavis.com>
2021-05-31 21:30:23 +0100

    Definition of make for indexed trees

--- leftist-heaps/leftist_heaps.v
+++ leftist-heaps/leftist_heaps.v
@@ -1,5 +1,9 @@
 (* Formalization of Leftist Heaps (Okasaki Ch.3) *)
 
+Require Export Nat.
+Require Export Arith.
+Require Export PeanoNat.
+
 (* The rank of a tree is the length of its rightmost path.
    In a leftist tree the rank of the left child must be
    larger or equal to the rank of the right child.
@@ -41,6 +45,56 @@ Inductive Leftist: Rank -> Data -> Set :=
                  (rrank: Rank)(z:Data)(rtree: Leftist rrank z),
           lrank >= rrank -> x <= y -> x <= z -> Leftist (S rrank) x.
 
+Definition ge_ge_dec: forall x y: nat, {x >= y} + {y >= x}.
+intros x y; case (le_ge_dec x y).
+intro hxy; right.
+auto.
+auto.
+Defined.
+
+Lemma ge_max_l: forall x y: nat, x >= y -> max x y = x.
+intros x y hxy.
+apply max_l.
+auto.
+Qed.
+
+Lemma ge_min_r: forall x y: nat, x >= y -> min x y = y.
+intros x y hxy.
+apply min_r.
+auto.
+Qed.
+
+Lemma ge_max_r: forall x y: nat, y >= x -> max x y = y.
+intros x y hyx.
+apply max_r.
+auto.
+Qed.
+
+Lemma ge_min_l: forall x y: nat, y >= x -> min x y = x.
+intros x y hyx.
+apply min_l.
+auto.
+Qed.
+
+
+(* The same as lnode, but we check the ranks on the fly *)
+Definition lmake:
+  forall (x:Data)
+         (lrank: Rank)(y:Data)(ltree: Leftist lrank y)
+         (rrank: Rank)(z:Data)(rtree: Leftist rrank z),
+         x <= y -> x <= z -> Leftist (S (min lrank rrank)) x.
+intros x lrank y ltree rrank z rtree hxy hxz.
+case (ge_ge_dec lrank rrank).
+
+intro hrank; rewrite ge_min_r.
+exact (lnode x lrank y ltree rrank z rtree hrank hxy hxz).
+exact hrank.
+
+intro hrank; rewrite ge_min_l.
+exact (lnode x rrank z rtree lrank y ltree hrank hxz hxy).
+exact hrank.
+Defined.
+
 (* We put all heaps in a single type *)
 
 Record LTree: Set := ltree
@@ -50,9 +104,9 @@ Record LTree: Set := ltree
   }.
 
 
-Definition ltSingle (x: Data): LTree := ltree 1 x (lsingle x).
+Definition lt_single (x: Data): LTree := ltree 1 x (lsingle x).
 
-Definition ltNode (left right: LTree): 
+Definition lt_node (left right: LTree): 
   forall (x:Data), 
     ltr_rank left >= ltr_rank right -> 
     x <= ltr_root left -> x <= ltr_root right -> LTree :=
@@ -69,15 +123,12 @@ Definition ltNode (left right: LTree):
 
 
 
-
-
-
 (* We add the empty heap to the structure *) 
 
 
 Definition LHeap: Set := option LTree.
 
-Definition lt_rank (t:LHeap): Rank :=
+Definition lh_rank (t:LHeap): Rank :=
   match t with
     None => 0
   | Some t => ltr_rank t
@@ -85,20 +136,4 @@ Definition lt_rank (t:LHeap): Rank :=
 
 (* The derived constructors. *)
 
-Definition lhnil: LHeap := None.
-
-
-(* Equational characterization of rank. *)
-
-Lemma rank_nil: lt_rank lhnil = 0.
-Proof.
-auto.
-Qed.
-
-(*
-Lemma rank_node: 
-  forall left right h, lt_rank (ltnode left right h) = S (lt_rank right).
-Proof.
-auto.
-Qed.
-*)
+Definition lh_empty: LHeap := None.
